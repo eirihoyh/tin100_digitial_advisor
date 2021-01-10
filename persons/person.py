9a573ff80e15011601d0368ae5043dee06c97c24
Eirik Høyheim <eirihoyh@nmbu.no>
2021-01-10 21:58:39 +0100

    started on the work with hoe the super class person should look like

---
+++ persons/person.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+
+class Person:
+    def __init__(self):
+        """
+        age
+        gender
+        status (student, married etc)
+        estimated sallary (mabye? need to see if I can find it by using api)
+        """
+        pass
+
+    def make_a_budget(self) -> Dict:
+        """
+        Will automatically make a budget. We need to make different catagories that the
+        user can relate to.
+        :return:
+        """
+        pass
+
+    def do_changes_to_budget(self) -> None:
+        pass
+
+    def check_if_beyond_salary(self) -> bool:
+        pass
