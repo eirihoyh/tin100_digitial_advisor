9c0a51696c1335e0b76e4c6cf537112eb390f197
Eirik Høyheim <eirihoyh@nmbu.no>
2021-01-12 12:19:17 +0100

    started on calculation of budget

--- persons/person.py
+++ persons/person.py
@@ -5,7 +5,7 @@ class Person:
     budget_setup = {
         "Food/drinks": 0,
         "Clothing": 0,
-        "Travell": 0,
+        "Travel": 0,
         "Payments": 0,
         "Buffer account": 0,
         "Freetime": 0
@@ -15,13 +15,19 @@ class Person:
                  avrg_income: Union[str, float],
                  monthly_debt: Union[str, float],
                  status_constant: Union[str, float]) -> None:
+
         self.nr_people = nr_people
         self.avrg_income = avrg_income
         self.monthly_debt = monthly_debt
         self.status_constant = status_constant
 
     def calculate_budget(self) -> None:
-        self.budget_setup["Clothing"] = self.nr_people * self.status_constant
+        self.budget_setup["Food/drinks"] = 3500 * self.status_constant * self.nr_people
+        self.budget_setup["Clothing"] = 500* self.nr_people * self.status_constant
+        self.budget_setup["Travel"]
+
+    def check_budget(self):
+        pass
 
     def check_if_beyond_salary(self) -> bool:
         amount = 0
@@ -39,18 +45,18 @@ class Student(Person):
         super().__init__(nr_people=nr_people,
                          avrg_income=avrg_income,
                          monthly_debt=monthly_debt,
-                         status_constant=1.25)
+                         status_constant=1)
 
 
 class Family(Person):
     def __init__(self, nr_people: int,
                  avrg_income: Union[str, float],
                  monthly_debt: Union[str, float]) -> None:
-        
+
         super().__init__(nr_people=nr_people,
                          avrg_income=avrg_income,
                          monthly_debt=monthly_debt,
-                         status_constant=1)
+                         status_constant=1.25)
 
 
 class Retired(Person):
