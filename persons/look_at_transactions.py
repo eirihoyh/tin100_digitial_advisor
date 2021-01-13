cfb014ae745cbad39a914274829d8855f87c6969
Eirik Høyheim <eirihoyh@nmbu.no>
2021-01-13 23:26:16 +0100

    added notes to Transaction class and added a "rolling bar" in gui

--- persons/look_at_transactions.py
+++ persons/look_at_transactions.py
@@ -1,14 +1,41 @@
-from typing import Any
+from typing import List
+
 
 class Transaction:
+    """
+    This class will only estimate if one single transaction is 'good' or 'bad' and will
+    not take into consideration that user is doing many small transactions
+    """
+    default_positive = "Good job on following budget!"
+    default_negative = "You are going over the budget!"
 
-    def __init__(self):
-        pass
+    tips = [
+        "Plan your week before purchasing groceries!",
+        "When visiting Oslo for a day, buy a 24 hours ticket. This will save "
+        "you 37kr every time you take public transportation after 3 rides",
+        "You can now save 27 500kr on your BSU account. This means that you can save 5 500kr on "
+        "your taxes!",
+        "If you earn less than 60 000kr this year you will not need to pay taxes!"
+    ]
+    def __init__(self, transaction_type):
 
-    def do_transaction(self):
+        if transaction_type == "Payments":
+            self.want_to_pay_debt = True
+        else:
+            self.want_to_pay_debt = False
+
+        self.transaction_type = transaction_type
+
+    def do_transaction(self) -> List[str]:
         """
-        Uses transaction money to
+        Uses dnb_transaction and the chosen categories from gui to estimate
+        how it's going with the budget
+        Need to get information on how much money have been spent (dnb_transaction), get
+        information about what it is spent on (self.transaction_type), relate
+        money spent to users budget (calculate_week_ratio) and need to give a message if
+        user is doing well or not and/or tips for further spendings.
         :return:
+        Returns a a list with two strings that contains a positive/negative message
         """
         pass
 
@@ -16,6 +43,14 @@ class Transaction:
         """
         Here I want to calculate to see if the user have gone beyond
         25% (one week) of it's budget
+        :return:
+        """
+        pass
+
+    def dnb_transaction(self):
+        """
+        here I want to find information about what transactions a user have been using
+        and relate it to transaction_type in database, ge
         :return:
         """
         pass
--- persons/transaction_gui.py
+++ persons/transaction_gui.py
@@ -9,7 +9,7 @@ frame0 = tk.Frame(master=root, width=100, height=100, borderwidth=5, relief=tk.S
 frame0.grid(row=0, column=0)
 
 frame0.rowconfigure(0, minsize=25, weight=1)
-frame0.columnconfigure([0, 1, 2, 3], minsize=50, weight=1)
+frame0.columnconfigure([0, 1], minsize=50, weight=1)
 
 transaction_types = ["Food", "Travel", "Payments"]
 
@@ -22,14 +22,8 @@ my_combo.bind("<<ComboboxSelected>>")
 my_combo.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 
 
-entry = tk.Entry(master=frame0)
-entry.grid(row=0,column=1, sticky="ew", padx=10, pady=10)
-
-label = tk.Label(master=frame0, text="Kr")
-label.grid(row=0, column=2, sticky="w", padx=10, pady=10)
-
 button = tk.Button(master=frame0, text="Make transaction")
-button.grid(row=0, column=3, sticky="nsew", padx=10, pady=10)
+button.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
 frame1 = tk.Frame(master=root, width=100, height=100, borderwidth=5)
 frame1.grid(row=1, column=0)
